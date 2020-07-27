7dbba83cd5adf1a5f728bc7262fe468e2d47970b
Ayaka Neko <neko@ayaka.moe>
2020-07-27 20:07:39 +0800

    :sparkles: 新增了主页面的样式和测试内容重置

---
+++ src/components/Header.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+      <header class="header home-fixed">
+        <a href="/" style="display: flex;align-items: center;flex: 1;">
+          <img id='logo' src="../assets/img/logo.png" style="height: 30px;"/>
+          <div id='divider'></div>
+          <img id='developer' src="../assets/img/developer.png" style="height: 14px;"/>
+        </a>
+        <div>
+          <el-button type="primary" class="login-btn">登陆</el-button>
+        </div>
+      </header>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+  components: {
+  },
+  props: {
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+
+  },
+  watch: {
+  },
+  created () {
+
+  },
+  mounted () {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+#logo {
+  margin-right: 10px;
+}
+
+#developer  {
+  margin-left: 11px;
+}
+
+#divider {
+  height: 30px;
+  width: 1px;
+  background-color: #DBDBDB;
+}
+
+header {
+  display: flex;
+  align-items: center;
+  padding-left: 40px;
+  border-bottom: 1px;
+  height:60px;
+  background:rgba(247,247,247,1);
+  box-shadow:0px 1px 0px 0px rgba(219,219,219,1);
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  z-index: 99;
+}
+
+.login-btn {
+  margin-right: 2rem;
+}
+
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div style="height: 100vh">
+      <el-menu default-active="1-4-1" class="el-menu-vertical">
+        <el-menu-item class="menu-item" index="1">
+          <img class="menu-icon" src="../assets/img/app.png" />
+          <span slot="title">应用 Application</span>
+        </el-menu-item>
+        <el-menu-item class="menu-item" index="2">
+          <img class="menu-icon" src="../assets/img/doc.png" />
+          <span slot="title">开发文档 Documentation</span>
+        </el-menu-item>
+      </el-menu>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.el-menu-vertical {
+  display: flex;
+  flex-direction: column;
+  margin-top: 1px;
+  padding-top: 40px;
+  padding-left: 40px;
+  width:350px;
+  min-height: 400px;
+  height: auto;
+  background:rgba(247,247,247,1);
+  box-shadow:1px 0px 0px 0px rgba(219,219,219,1);
+  position: fixed;
+  top: 60px;
+  left: 0;
+  bottom: 0;
+}
+
+.menu-item {
+  font-size:16px;
+  font-family:PingFangSC-Medium,PingFang SC, Arial, Helvetica, sans-serif;
+  font-weight:500;
+  display: flex;
+  align-items: center;
+  width:270px;
+  height:40px;
+  background:rgba(247,247,247,1);
+  border-radius:4px;
+  margin-bottom: 0.6rem;
+}
+
+.menu-icon {
+  height: 20px;
+  margin-right: 10px;
+}
+
+.is-active {
+  color: #542DE0;
+  background:rgba(219,219,219,1);
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  text-decoration: none;
+  color: #333333;
+}
+</style>
