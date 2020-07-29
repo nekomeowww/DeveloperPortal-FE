ee95c05dba27d387ecfd06607156c63ae560d23b
Ayaka Neko <neko@ayaka.moe>
2020-07-29 18:38:05 +0800

    :sparkles: 新增了登陆界面

---
+++ src/components/Login/Input.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="input">
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="ruleForm">
+      <el-form-item prop="email">
+        <el-input type="email" placeholder="请输入电子邮箱..." v-model="ruleForm.email" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item prop="pass">
+        <el-input type="password" placeholder="请输入密码..." v-model="ruleForm.pass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')" style="width: 100%;">登陆</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    let validateEmail = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入电子邮箱地址'))
+      } else {
+        if (!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value)) {
+          callback(new Error('电子邮箱格式不正确'))
+        }
+        callback()
+      }
+    }
+    return {
+      ruleForm: {
+        email: '',
+        pass: ''
+      },
+      rules: {
+        email: [
+          { required: true, validator: validateEmail, trigger: 'blur' }
+        ],
+        pass: [
+          { required: true, message: '密码不能为空', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
