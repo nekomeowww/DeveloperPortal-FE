b6762a47f2fa2fa894d36e0c957431a0e86c8e56
Ayaka Neko <neko@ayaka.moe>
2020-08-03 19:07:39 +0800

    :zap: 对每个页面进行用户登录情况检查
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/page/NewApplication.vue
+++ src/page/NewApplication.vue
@@ -1,23 +1,53 @@
 <template>
     <div>
-      <Header />
+      <Header
+        :showLoginBtn="!isLoggedIn"
+      />
       <Menu />
       <NewAppForm />
     </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex'
+import Axios from 'axios'
 
 import Header from '@/components/Header.vue'
 import Menu from '@/components/Menu.vue'
 import NewAppForm from '@/components/NewAppForm.vue'
+
+import { getCookie, disassemble } from '../util/cookie'
+import env from '../../env.json'
 
 export default {
   components: {
     Header,
     Menu,
     NewAppForm
-  }
+  },
+  computed: {
+    ...mapState(['isLoggedIn'])
+  },
+  methods: {
+    ...mapActions(['setLoggedIn'])
+  },
+  mounted () {
+    const c = getCookie('ACCESS-TOKEN')
+    if (c) {
+      const res = disassemble(c)
+      res.status = true
+      this.setLoggedIn(res)
+    }
 
+    Axios.get(env.DEVELOPERAPI + '/user/app?id=' + this.userId).then(apps => {
+      if (apps.data.id) {
+        let id = apps.data.id
+        id = id + 1
+        this.setCurrentAppId(id)
+      } else {
+        this.setCurrentAppId(1)
+      }
+    })
+  }
 }
 </script>
