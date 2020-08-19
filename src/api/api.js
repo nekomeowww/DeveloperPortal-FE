13fe0bdb962109ce70de353b0a1994d93b775b2d
Ayaka Neko <neko@ayaka.moe>
2020-08-19 22:04:41 +0800

    :sparkles: 完成 Team 头像和 Team App 头像的上传处理工作
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/api/api.js
+++ src/api/api.js
@@ -22,5 +22,15 @@ export default {
       url: url,
       data: formdata
     })
+  },
+  uploadTeamAppImage (data, id, userId, appId) {
+    const url = env.DEVELOPERAPI + '/user/' + userId + '/team/' + id + '/app/' + appId + '/uploadTeamAppIcon'
+    const formdata = new FormData()
+    formdata.append('image', data)
+    return Axios({
+      method: 'POST',
+      url: url,
+      data: formdata
+    })
   }
 }
