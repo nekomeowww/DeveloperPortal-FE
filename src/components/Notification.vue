347b04fd89abf0c7866ef438684d5c4bc155ab66
Ayaka Neko <neko@ayaka.moe>
2020-08-21 16:19:41 +0800

    :sparkles: 显示通知列表和通知内容
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -9,13 +9,61 @@
       </div>
       <div class="notification-key-value">
         <div v-for="(value, index) in notifyData" :key=index>
-          <div v-if="index === 0" class="notification-container" style="padding-top: 12px">
-            <div class="notification-name"><i class="el-icon-lock"></i> {{ value.key }}</div>
+          <div v-if="index === 0" class="notification-container" :id="value.notifyGlobalId" style="padding-top: 12px">
+            <div class="info-container">
+              <div class="notification-name-container">
+              <div class="inviter-container">
+                <img class="inviter-avatar" :src="value.inviter.avatar">
+                <div class="inviter-name-container">
+                  <span class="nickname">{{ value.inviter.nickname }}</span>
+                  <span class="email">{{ value.body }}</span>
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="notification-team-container">
+              <div class="team-container">
+                <img class="team-img" :src="value.team.img">
+                  <div class="team-name-container">
+                    <span class="join">加入</span>
+                    <span class="name">{{ value.team.name }}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
             <div class="notification-mid"></div>
+            <div class="selection-container">
+              <el-button type="success" icon="el-icon-check" circle @click="() => { confirm(index, value.notifyGlobalId, value.userId, value.notifyId, value.teamId) }"></el-button>
+              <el-button type="danger" icon="el-icon-close" circle @click="() => { deny(index, value.notifyGlobalId, value.userId, value.notifyId, value.teamId) }"></el-button>
+            </div>
           </div>
-          <div v-if="index !== 0" class="notification-container">
-            <div class="notification-name"><i class="el-icon-lock"></i> {{ value.key }}</div>
+          <div v-if="index !== 0" :id="value.notifyGlobalId" class="notification-container">
+            <div class="info-container">
+              <div class="notification-name-container">
+              <div class="inviter-container">
+                <img class="inviter-avatar" :src="value.inviter.avatar">
+                <div class="inviter-name-container">
+                  <span class="nickname">{{ value.inviter.nickname }}</span>
+                  <span class="email">{{ value.body }}</span>
+                </div>
+              </div>
+            </div>
+            <br>
+            <div class="notification-team-container">
+              <div class="team-container">
+                <img class="team-img" :src="value.team.img">
+                  <div class="team-name-container">
+                    <span class="join">加入</span>
+                    <span class="name">{{ value.team.name }}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
             <div class="notification-mid"></div>
+            <div class="selection-container">
+              <el-button type="success" icon="el-icon-check" circle @click="() => { confirm(index, value.notifyGlobalId, value.userId, value.notifyId, value.teamId) }"></el-button>
+              <el-button type="danger" icon="el-icon-close" circle @click="() => { deny(index, value.notifyGlobalId, value.userId, value.notifyId, value.teamId) }"></el-button>
+            </div>
           </div>
         </div>
       </div>
@@ -47,7 +95,45 @@ export default {
     ...mapState(['currentAppId'])
   },
   methods: {
-    ...mapActions(['setLoggedIn', 'setCurrentAppId'])
+    ...mapActions(['setLoggedIn', 'setCurrentAppId']),
+    confirm (index, id, uid, noId, teamId) {
+      const elem = document.getElementById(id)
+      let opacity = 0
+      // eslint-disable-next-line no-use-before-define
+      const intervalId = setInterval(frame, 5)
+      function frame () {
+        if (opacity === 100) {
+          clearInterval(intervalId)
+        } else {
+          opacity++
+          elem.style.opacity = opacity / 100
+        }
+      }
+      this.notifyData.splice(index, 1)
+      this.acceptInvite({ userId: uid, notifyId: noId, teamId: teamId })
+    },
+    deny (index, id, uid, noId, teamId) {
+      const elem = document.getElementById(id)
+      let opacity = 0
+      // eslint-disable-next-line no-use-before-define
+      const intervalId = setInterval(frame, 5)
+      function frame () {
+        if (opacity === 100) {
+          clearInterval(intervalId)
+        } else {
+          opacity++
+          elem.style.opacity = opacity / 100
+        }
+      }
+      this.notifyData.splice(index, 1)
+      this.denyInvite({ userId: uid, notifyId: noId, teamId: teamId })
+    },
+    acceptInvite ({ userId, notifyId, teamId }) {
+      Axios.get(env.DEVELOPERAPI + '/invite/update?id=' + this.userId + '&result=accept&notifyId=' + notifyId + '&inviteId=' + userId + '&teamId=' + teamId)
+    },
+    denyInvite ({ userId, notifyId, teamId }) {
+      Axios.get(env.DEVELOPERAPI + '/invite/update?id=' + this.userId + '&result=deny&notifyId=' + notifyId + '&inviteId=' + userId + '&teamId=' + teamId)
+    }
   },
   created () {
     const c = getCookie('ACCESS-TOKEN')
@@ -65,9 +151,24 @@ export default {
       })
       this.$router.push({ name: 'Home' })
     }
-    Axios.get(env.DEVELOPERAPI + '/notification/push?id=' + this.userId).then(res =>
-      console.log(res.data)
-    )
+    Axios.get(env.DEVELOPERAPI + '/notification/push?id=' + this.userId).then(res => {
+      const notifications = res.data
+      notifications.forEach(notification => {
+        notification.team = { img: '', name: '' }
+        notification.inviter = { avatar: '', nickname: '', email: '' }
+
+        Axios.get(env.DEVELOPERAPI + '/team/detail?teamId=' + notification.teamId).then(teamres => {
+          notification.team.img = env.DEVELOPERAPI + '/img/' + teamres.data.img
+          notification.team.name = teamres.data.detail.name
+          Axios.get(env.DEVELOPERAPI + '/user/info?id=' + notification.userId).then(userres => {
+            notification.inviter.avatar = env.MTTKIMGCDN + userres.data.avatar
+            notification.inviter.nickname = userres.data.nickname
+            notification.inviter.email = userres.data.email
+            this.notifyData.push(notification)
+          })
+        })
+      })
+    })
   },
   mounted () {
   }
@@ -113,16 +214,79 @@ export default {
   padding-bottom: 12px;
 }
 
-.notification-name {
-  font-size: 1.2rem;
-  font-weight: 600;
-  color: #22863A;
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 1;
-  overflow: hidden;
-  word-break: break-all;
+.info-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.notification-name-container {
   margin-right: 5px;
+}
+
+.inviter-container {
+  display: flex;
+  align-items: center;
+}
+
+.inviter-name-container {
+  margin-left: 1rem;
+  display: flex;
+  flex-direction: column;
+  .nickname {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    overflow: hidden;
+    word-break: break-all;
+  }
+  .email {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    overflow: hidden;
+    word-break: break-all;
+    color: #B2B2B2;
+  }
+}
+
+.inviter-avatar {
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+.team-container {
+  display: flex;
+  align-items: center;
+}
+
+.team-name-container {
+  margin-left: 1rem;
+  display: flex;
+  flex-direction: column;
+  .join {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    overflow: hidden;
+    word-break: break-all;
+    color: #B2B2B2;
+  }
+  .name {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    overflow: hidden;
+    word-break: break-all;
+  }
+}
+
+.team-img {
+  width: 48px;
+  height: 48px;
+  object-fit: cover;
+  border-radius: 5px;
 }
 
 /deep/ .el-icon-lock {
